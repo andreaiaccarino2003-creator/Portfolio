494a6825b6d454234e88552a7727d40f8cc292ae
Andrea Iaccarino <andreaiaccarino@unknown.home>
2025-08-14 13:47:26 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./portfolio.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Andrea Carlo Iaccarino - Film Producer</h1>
+
+      <section>
+        <h2>Out of Sight - Short Film</h2>
+        <a href="https://youtu.be/eLlmQh4TKIg?si=b8kOrBMIX3UMHKM4" target="_blank">
+          Watch here
+        </a>
+      </section>
+
+      <section>
+        <h2>Devil in a Bottle - Music Video</h2>
+        <a href="https://www.youtube.com/watch?v=DyQhEV_D0PE" target="_blank">
+          Watch here
+        </a>
+      </section>
+
+      <section>
+        <h2>Papa - Short Film</h2>
+        <a href="https://youtu.be/KKVFer_1p6w?si=-PU_WKfUVLaKJDC5" target="_blank">
+          Watch here
+        </a>
+      </section>
+    </div>
+  );
+}
+
+export default App;
+
